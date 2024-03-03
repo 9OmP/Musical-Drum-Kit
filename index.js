aca7f259ae134051f07b7ef9d8e1029d2e24b916
9OmP <ompatil16022002@gmail.com>
2024-03-03 20:28:19 +0530

    uploaded initial files

---
+++ index.js
@@ -0,0 +1,15 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+for(var i = 0 ; i<numberOfButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener('click', function (){
+        var button_name = this.innerHTML;
+        console.log(button_name);
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    
+    });
+    
+}
+
+
+
