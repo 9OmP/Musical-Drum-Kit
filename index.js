c6b3258f64be28f8e43435c31cf229a5ec490e5d
9OmP <ompatil16022002@gmail.com>
2024-03-05 23:52:06 +0530

    update index.js

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ for(var i = 0 ; i<numberOfButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener('click', function (){
         var button_name = this.innerHTML;
-        console.log(button_name);
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+
+        switch (key) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+
+
+
+        // console.log(button_name);
+        // var audio = new Audio("sounds/tom-1.mp3");
+        // audio.play();
     
     });
-    
 }
-
-
 
